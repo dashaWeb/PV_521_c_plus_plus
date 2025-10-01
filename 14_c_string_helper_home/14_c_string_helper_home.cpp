78840d3507e667f64c420c86f8ca60078061638d
Dasha <dashakonopelko@gmail.com>
2025-10-01 20:06:20 +0300

    mult array

---
+++ 14_c_string_helper_home/14_c_string_helper_home.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+
+
+char* insertSymbol(char* str, char s, int index)
+{
+    if (index < 0 or index > strlen(str)) {
+        cout << "Enter index" << endl;
+        return nullptr;
+    }
+    char* tmp = new char[strlen(str) + 2];
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (i < index) {
+            tmp[i] = str[i];
+        }
+        else {
+            tmp[i + 1] = str[i];
+        }
+    }
+    tmp[index] = s;
+    tmp[strlen(str) + 1] = '\0';
+    return tmp;
+}
+
+void insertSymbolStat(char* str, char s, int index)
+{
+    if (index < 0 or index > strlen(str)) {
+        cout << "Enter index" << endl;
+        return;
+    }
+    int size = strlen(str);
+    for (size_t i = strlen(str); i > index; i--)
+    {
+        str[i] = str[i - 1];
+    }
+    str[index] = s;
+    str[size + 1] = '\0';
+}
+
+void task6(char* str)
+{
+    int counter_letter = 0;
+    int counter_digit = 0;
+    int counter_other = 0;
+
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (isalpha(str[i])) counter_letter++;
+        //else if (isdigit(str[i])) counter_digit++;
+        else if (str[i] >= '0' and str[i] <= '9') counter_digit++;
+        else counter_other++;
+    }
+
+    cout << "Number of letters :: " << counter_letter << endl;
+    cout << "Number of digits  :: " << counter_digit << endl;
+    cout << "Number of other   :: " << counter_other << endl;
+
+}
+
+int main()
+{
+    /*Завдання 3
+        Написати функцію, яка вставляє в рядок у вказану позицію заданий символ.*/
+
+    char str[100], s;
+    int index;
+    cout << "Enter line :: ";
+    cin.getline(str, 100);
+    cout << "Enter symbol :: ";
+    cin >> s;
+    cout << "Enter position :: ";
+    cin >> index;
+    cout << str << endl;
+    auto new_str = insertSymbol(str, s, index);
+    cout << new_str << endl;
+
+    insertSymbolStat(str, '^', 5);
+    cout << str << endl;
+
+    //Завдання 6
+    //    Користувач вводить рядок.Визначити кількість літер, кількість цифр і кількість інших символів, присутніх у рядку.
+    task6(str);
+}
---
+++ 15_multi_array_dynamic/15_multi_array_dynamic.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+template <typename T>
+void fill(T** arr, const int& row, const int& col, const int& min = 1, const int& max = 10)
+{
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            arr[i][j] = min + rand() % (max - min + 1);
+        }
+    }
+}
+
+
+template <typename T>
+void print(T** arr, const int& row, const int& col, const string& prompt = "")
+{
+
+    cout << (prompt.empty() ? "" : prompt + " :: \n");
+
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+template <typename T>
+void fillArray(T* arr, size_t size, int min = 1, int max = 2)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = min + rand() % (max - min + 1);
+    }
+}
+
+//Написати функцію, що додає рядок двовимірному масиву в кінець.
+//Написати функцію, що додає рядок двовимірному масиву на початок.
+//Написати функцію, що додає рядок двовимірному масиву в зазначену позицію.
+template<typename T>
+void insert(T**& arr, int& row, const int& col, const int& index)
+{
+    if (index < 0 or index > row) {
+        cout << "Error index" << endl;
+        return;
+    }
+    T** tmp = new T * [row + 1];
+    for (size_t i = 0; i < row; i++)
+    {
+        if (i < index) {
+            tmp[i] = arr[i];
+        }
+        else {
+            tmp[i + 1] = arr[i];
+        }
+    }
+    tmp[index] = new T[col]{};
+
+    row++;
+    delete[] arr;
+    arr = tmp;
+}
+// 
+// 
+//Написати функцію, що видаляє рядок двовимірного масиву за вказаним номером.
+template <typename T>
+void remove(T**& arr, int& row, const int& index)
+{
+    if (index < 0 or index >= row) {
+        cout << "Error index" << endl;
+        return;
+    }
+    T** tmp = new T * [--row];
+    for (size_t i = 0; i < row; i++)
+    {
+        if (i < index) {
+            tmp[i] = arr[i];
+        }
+        else {
+            tmp[i] = arr[i + 1];
+        }
+    }
+    delete[] arr[index];
+    delete[] arr;
+
+    arr = tmp;
+}
+
+//Написати функцію, що видаляє рядок двовимірного масиву в кінці.
+//Написати функцію, що видаляє рядок двовимірного масиву на початку.
+
+int main()
+{
+    
+    int row = 4, col = 6;
+
+    // create mult array
+    int** arr = new int* [row];
+
+    for (size_t i = 0; i < row; i++)
+    {
+        arr[i] = new int[col];
+    }
+
+    fill(arr, row, col);
+    print(arr, row, col, "Print Array ");
+
+    cout << endl;
+
+    insert(arr, row, col, 2);
+    print(arr, row, col, "Print insert ");
+
+
+    remove(arr, row, 1);
+    print(arr, row, col, "Print remove ");
+
+    // delete mult array
+    for (size_t i = 0; i < row; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+    arr = nullptr;
+
+
+
+}
